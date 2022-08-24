308df1476fd66da881d92946fb7fe305c965ed3a
Muhammad Ammar <ammartoonice@gmail.com>
2022-08-24 21:26:59 +0500

    Add files via upload

---
+++ js-basic/basic.js
@@ -0,0 +1,27 @@
+//ways to print in js
+//console.log("hello world")
+//alert("green here")
+// document.write("green angry")
+
+// document.write("green furious")
+
+// document.write("green furious"," ", 4 + 5," ", "green attcked")
+
+// API : application program interface data comes in form of json
+// console.warn("destroy green :warn")
+// console.error("green killed : error")
+
+var number1
+
+
+
+
+
+
+
+
+
+
+
+
+
